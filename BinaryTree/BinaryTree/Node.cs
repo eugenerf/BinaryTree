54a7af6dbeabfd22af139c423f24408b6687ec09
eugene <eugenerf@mail.ru>
2020-06-01 13:44:55 +0300

    Added tree from array generation and ShowByLevel.

--- BinaryTree/BinaryTree/Node.cs
+++ BinaryTree/BinaryTree/Node.cs
@@ -1,4 +1,6 @@
-﻿namespace BinaryTree
+﻿using System;
+
+namespace BinaryTree
 {
     class Node
     {
@@ -18,6 +20,43 @@
             for (int i = 1; i < values.Length; i++)
             {
                 AddValue(values[i]);
+            }
+        }
+
+        public void ShowByLevel()
+        {
+            string[] levelsStrings = new string[1];
+            GenerateLevelStrings(ref levelsStrings);
+            for (int i = levelsStrings.Length - 1; i >= 0; i--)
+            {
+                Console.Write($"Level {i + 1}: ");
+                Console.WriteLine(levelsStrings[i]);
+            }
+        }
+
+        private void GenerateLevelStrings(ref string[] levelsStrings, int currentLevel = 0)
+        {
+            int currentLength = (levelsStrings == null) ? 0 : levelsStrings.Length;
+            if (currentLevel >= currentLength)
+            {
+                Array.Resize(ref levelsStrings, currentLevel + 1);
+            }
+            if (levelsStrings[currentLevel] == null)
+            {
+                levelsStrings[currentLevel] = "";
+            }
+            if(levelsStrings[currentLevel].Length > 0)
+            {
+                levelsStrings[currentLevel] += ", ";
+            }
+            levelsStrings[currentLevel] += Value.ToString();
+            if (Left != null)
+            {
+                Left.GenerateLevelStrings(ref levelsStrings, currentLevel + 1);
+            }
+            if (Right != null)
+            {
+                Right.GenerateLevelStrings(ref levelsStrings, currentLevel + 1);
             }
         }
 
